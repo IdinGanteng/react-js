0e3922d1d22d9e27fe710792972eac83f619fe78
IdinGanteng <kanggayus101@gmail.com>
2023-03-18 01:15:30 +0700

    gjshdgj

--- src/service/api.js
+++ src/service/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 
-const url = "http://127.0.0.1:3003/user";
+const url = "https://127.0.0.1:3003/user";
 
 export const getallUsers = async (id) => {
     id = id || '';
