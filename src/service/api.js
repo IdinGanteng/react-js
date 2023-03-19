76c168fcd614e96c8b472d6ab2e6085ac343e0c7
IdinGanteng <kanggayus101@gmail.com>
2023-03-19 07:14:56 +0700

    apakah berhasil

--- src/service/api.js
+++ src/service/api.js
@@ -18,7 +18,7 @@ import axios from 'axios';
 
 const url = "http://127.0.0.1:3003/user";
 
-export const getallUsers = async (id,res) => {
+export const getallUsers = async (id) => {
     id = id || '';
     return await axios.get(`${url}/${id}`);
 }
