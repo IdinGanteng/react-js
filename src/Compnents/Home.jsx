d370877187b6a7b40bf8ee35dd8d4c8550256d6f
IdinGanteng <kanggayus101@gmail.com>
2023-04-20 03:19:12 +0700

    scan

--- src/Compnents/Home.jsx
+++ src/Compnents/Home.jsx
@@ -1,10 +1,33 @@
 import React from 'react';
-
+import { useRef } from 'react';
+import QrScanner from 'react-qr-scanner';
 
 
 const Home = () => {
+    const qrScannerRef = useRef(null);
+
+    const handleScan = data => {
+      if (data) {
+        // Proses data hasil pemindaian QR code di sini
+        console.log(data);
+      }
+    }
+  
+    const handleError = err => {
+      // Tangani kesalahan pemindai QR code di sini
+      console.error(err);
+    }
     return (
+        <>
         <h1>admin payment pdam</h1>
+        <QrScanner
+        ref={qrScannerRef}
+        delay={300}
+        onError={handleError}
+        onScan={handleScan}
+        style={{ width: '100%' }}
+      />
+      </>
     )
 }
 
